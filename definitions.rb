b013b9cee449369a24fc608b3716a26dbf9af869
Theron Boerner <hunterboerner@gmail.com>
2013-10-21 12:53:48 -0500

    content!

--- definitions.rb
+++ definitions.rb
@@ -2,13 +2,23 @@ require "./helpers.rb"
 
 module RecipeDefinitions
   include RecipesModule
+  Recipes.new_recipe(:crystallizedQuantonium, Smelting.new("rawQuantonium",
+   "crystallizedQuantonium", 1))
   # Recipes.new_recipe(:quantumOre, Smelting.new("cobblestone", "stone", 1))
   # Recipes.new_recipe(:dirt, Crafting.new("stone", 1, ["stick", "stone", "","stick", "stone", "","stone", "stone", ""]))
 end
 
 module Localizations
   @@wiki_files = Dir["views/wiki/**/*.qc"]
-  @@localized = {a_meta: "Meta", blocks: "Blocks", items: "Items", index: "Index"}
+  @@localized = {
+    a_meta: "Meta",
+    blocks: "Blocks",
+    items: "Items",
+    index: "Home",
+    quantoniumOre: "Quantonium Ore",
+    rawQuantonium: "Raw Quantonium",
+    crystallizedQuantonium: "Crystallized Quantonium"
+  }
   def localized
     @@localized
   end
