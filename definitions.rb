d7b086db5b521fcc410fc96662af28df9d9078e7
hunterboerner <hunterboerner@gmail.com>
2013-11-20 12:14:19 -0600

    stupid me

--- definitions.rb
+++ definitions.rb
@@ -35,7 +35,7 @@ module Localizations
     :IOT => "ION Tunneler",
     :IOS => "ION Scanner",
     :IOH => "ION Harvester",
-    guide: "Getting Started"
+    guide: "Getting Started",
     mcForumPost: "Minecraft Forum Post"
   }
   def localized
