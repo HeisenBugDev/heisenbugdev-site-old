3ac31c6585ba2aa906305a699f54ab3cb0f407f3
hunterboerner <hunterboerner@gmail.com>
2013-12-12 22:08:48 -0600

    support other builds

--- build_handler.rb
+++ build_handler.rb
@@ -4,14 +4,14 @@ require 'net/http'
 module BuildHandler
   JENKINS_URI = URI.parse('http://ci.theronsminecraft.com')
 
-  def get_downloads
-    get_json['builds']
+  def get_downloads(job)
+    get_json(job)['builds']
   end
 
-  def get_json(job=nil)
+  def get_json(job, build=nil)
     begin
       http     = Net::HTTP.new(JENKINS_URI.host, JENKINS_URI.port)
-      request  = Net::HTTP::Get.new("/job/QuantumCraft/#{job}/api/json")
+      request  = Net::HTTP::Get.new("/job/#{job}/#{build}/api/json")
       response = http.request(request)
       JSON.parse(response.body)
     rescue SocketError
@@ -19,14 +19,14 @@ module BuildHandler
     end
   end
 
-  def get_version(file_name_rec)
+  def get_version(file_name_rec, name_of_mod)
     file_name     = file_name_rec + ''
     mc_version    = get_mc_version(file_name).to_s
     wrapped_words = get_wrapped_words(file_name).to_s.reverse.sub!('-', '').reverse
     file_name.sub!(mc_version, '')
     file_name.sub!(wrapped_words, '')
-    file_name.sub!('QuantumCraft', '')
-    file_name.sub!('quantumcraft','')
+    file_name.sub!(name_of_mod, '')
+    file_name.sub!(name_of_mod.to_s.downcase,'')
     file_name.sub!('.jar', '')
   end
 
