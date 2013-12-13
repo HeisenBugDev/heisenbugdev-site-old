c0d9b55b1397aae4bc0f26ea54b119f1e8150770
hunterboerner <hunterboerner@gmail.com>
2013-12-13 12:28:50 -0600

    recipe

--- definitions.rb
+++ definitions.rb
@@ -3,43 +3,56 @@ require_relative "helpers"
 module RecipeDefinitions
   include RecipesModule
   Recipes.new_recipe(:crystallizedQuantonium, Smelting.new("rawQuantonium",
-   "crystallizedQuantonium", 1))
+                                                           "crystallizedQuantonium", 1))
+  Recipes.new_recipe(:QCP, Crafting.new("quantumPlating",
+                                                   1,
+                                                   ['ingotUnbioxenium',
+                                                    'ingotUniboxenium',
+                                                    'ingotUniboxenium',
+                                                    'ingotUniboxenium',
+                                                    'crystalQuantonium',
+                                                    'ingotUniboxenium',
+                                                    'ingotUniboxenium',
+                                                    'ingotUniboxenium',
+                                                    'ingotUniboxenium']))
   # Recipes.new_recipe(:quantumOre, Smelting.new("cobblestone", "stone", 1))
-  # Recipes.new_recipe(:dirt, Crafting.new("stone", 1, ["stick", "stone", "","stick", "stone", "","stone", "stone", ""]))
+  Recipes.new_recipe(:QCP, Crafting.new("stone", 1, ["stick", "stone", "","stick", "stone", "","stone", "stone", ""]))
 end
 
 module Localizations
   @@wiki_files = Dir["views/wiki/**/*.qc"]
-  @@localized = {
-    a_meta: "Meta",
-    blocks: "Blocks",
-    items: "Items",
-    development: "Development",
-    index: "Home",
-    machines: "Machines",
-    quantoniumOre: "Quantonium Ore",
-    rawQuantonium: "Raw Quantonium",
-    crystallizedQuantonium: "Crystallized Quantonium",
-    quantumTools: "Quantum Tools",
-    quantumPortableCapicator: "Quantum Portable Capicator",
-    unbioxeniumOre: "Unbioxenium Ore",
-    nameDatabase: "Name Database",
-    :QDE => "Quantum De-Energizer",
-    :QEI => "Quantum Energy Injector",
-    :QEE => "Quantum Energy Extractor",
-    :QDM => "Quantum De-Materializer",
-    :QDS => "Quantum Dislocator",
-    :QDL => "Quantum De-Electrifier",
-    :QEL => "Quantum Electrifier",
-    :IOF => "ION Forge",
-    :IOT => "ION Tunneler",
-    :IOS => "ION Scanner",
-    :IOH => "ION Harvester",
-    :QCP => "Quantum Capacitor",
-    multiTool: "Multi Tool",
-    guide: "Getting Started",
-    mcForumPost: "Minecraft Forum Post"
+  @@localized  = {
+      a_meta:                   "Meta",
+      blocks:                   "Blocks",
+      items:                    "Items",
+      development:              "Development",
+      index:                    "Home",
+      machines:                 "Machines",
+      quantoniumOre:            "Quantonium Ore",
+      rawQuantonium:            "Raw Quantonium",
+      crystallizedQuantonium:   "Crystallized Quantonium",
+      quantumTools:             "Quantum Tools",
+      quantumPortableCapicator: "Quantum Portable Capicator",
+      unbioxeniumOre:           "Unbioxenium Ore",
+      nameDatabase:             "Name Database",
+      :QDE                      => "Quantum De-Energizer",
+      :QEI                      => "Quantum Energy Injector",
+      :QEE                      => "Quantum Energy Extractor",
+      :QDM                      => "Quantum De-Materializer",
+      :QDS                      => "Quantum Dislocator",
+      :QDL                      => "Quantum De-Electrifier",
+      :QEL                      => "Quantum Electrifier",
+      :IOF                      => "ION Forge",
+      :IOT                      => "ION Tunneler",
+      :IOS                      => "ION Scanner",
+      :IOH                      => "ION Harvester",
+      :QCP                      => "Quantum Capacitor",
+      general:                  "General",
+      multiTool:                "Multi Tool",
+      guide:                    "Getting Started",
+      mcForumPost:              "Minecraft Forum Post"
   }
+
   def self.localized
     @@localized
   end
