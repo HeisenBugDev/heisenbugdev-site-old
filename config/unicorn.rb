636df35e8694ac8642881f5df27ec352d6c51c55
hunterboerner <hunterboerner@gmail.com>
2013-12-17 10:32:56 -0600

    unicorn!

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,5 +3,5 @@ timeout 30
 preload_app true
 
 before_fork do |server, worker|
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2 -r ./app.rb")
 end
