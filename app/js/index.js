07146b645972412640d439dadc2623e643e30b39
hunterboerner <hunterboerner@gmail.com>
2013-12-19 20:01:43 -0600

    fix for firefox too

--- app/js/index.js
+++ app/js/index.js
@@ -3,8 +3,11 @@ window.onload = (function () {
         var canvas = document.createElement('canvas');
         canvas.width = img.width;
         canvas.height = img.height;
+        canvas.className = '.no-aa.image';
         var ctx = canvas.getContext('2d');
         ctx.webkitImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+        ctx.mozImageSmoothingEnabled = false;
         ctx.drawImage($(img)[0], 0, 0, canvas.width, canvas.height);
         $(img)[0].parentNode.replaceChild(canvas, $(img)[0]);
         //$(img).replaceWith(canvas);
