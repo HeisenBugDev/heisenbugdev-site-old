5f8f5c0e4e40c3cf18484b30efe6400dd5a0e4a8
hunterboerner <hunterboerner@gmail.com>
2013-12-21 09:37:31 -0600

    collecting info about the status of the builds to use for coloring the buttons

--- app/workers/download_fetcher.rb
+++ app/workers/download_fetcher.rb
@@ -15,20 +15,23 @@ class DownloadFetcher
   recurrence { minutely }
 
   def perform
-    puts "I am performing!"
+    puts 'I am performing!'
     self.class.numbers = []
     self.class.files   = []
     file_info_builder  = []
     numbers_builder    = []
     files_builder      = []
+    status_builder     = []
     file_names         = DownloadsManager.file_names
     DownloadsManager.names.each_with_index do |name, name_index|
       tmp_numbers = []
       tmp_files   = []
+      tmp_status  = []
       get_downloads(name).each_with_index do |build, index|
         unless index > 1
           tmp_numbers << build['number']
           tmp_files << get_json(name, build['number'])['artifacts']
+          tmp_status << get_json(name, build['number'])['actions'][5]['level']
         end
       end
       numbers_builder << tmp_numbers
@@ -43,10 +46,37 @@ class DownloadFetcher
         files_info << file_info
       end
       file_info_builder << files_info
+
+      statuses = []
+      tmp_status.each_with_index do |status, index|
+        single_row_status = []
+        files_info[index][:types].each do |info|
+          clazz = ''
+          unless info.include? 'universal'
+            clazz << 'tiny '
+          end
+          if status.nil?
+            clazz << 'blue'
+          else
+            if status.include? 'Bug'
+              clazz << 'red'
+            elsif status.include? 'Release'
+              clazz << 'green'
+            elsif status.include? 'Beta'
+              clazz << 'teal'
+            end
+          end
+          single_row_status << clazz
+        end
+        statuses << single_row_status
+      end
+      status_builder << statuses
     end
+
     REDIS.set('file_info', file_info_builder.to_json)
     REDIS.set('numbers', numbers_builder.to_json)
     REDIS.set('files', files_builder.to_json)
+    REDIS.set('colors', status_builder.to_json)
     self.class.file_info = file_info_builder
     self.class.numbers   = numbers_builder
     self.class.files     = files_builder
