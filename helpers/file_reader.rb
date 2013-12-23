f3bb7490376859d1bababaae1e542ce0bc2f8d0c
hunterboerner <hunterboerner@gmail.com>
2013-12-23 14:50:36 -0600

    use the fancy data from the github readme

--- helpers/file_reader.rb
+++ helpers/file_reader.rb
@@ -1,10 +1,12 @@
 require_relative '../config/definitions'
+require 'github/markup'
 module FileReader
   file_path = ""
 
 
   class WikiFiles
     include Localizations
+
     def self.localized
       @@localized
     end
@@ -12,32 +14,38 @@ module FileReader
 
   def output(file_path)
     if file_path.reverse[0] == "/"
-      file_path = file_path.reverse.sub!("/","").reverse
+      file_path = file_path.reverse.sub!("/", "").reverse
     end
-    file_string = File.open("views/#{file_path}.qc".gsub!("//","/"), "r").read
-    file_name = File.basename(file_path, File.extname(file_path))
-    html = "<div class=\"twelve wide right floated column\">"
-    html << "<h1>"
-    html << WikiFiles.localized[file_name.to_sym]
-    if File.exist?("public/images/wiki/#{file_name}.png")
-      html << "<img class=\"no-aa-image\" width=\"10%\" height=\"auto\" src=\"#{url("images/wiki/#{file_name}.png")}\"/>"
+    file_name   = File.basename(file_path, File.extname(file_path))
+    file_string = GitHub::Markup.render("#{file_name}.md", File.open("views/#{file_path}.qc".gsub!("//", "/"), "r").read)
+    html        = "<div class=\"twelve wide right floated column\">"
+    unless file_name == 'index'
+      html << "<h1>"
+      html << WikiFiles.localized[file_name.to_sym]
+      if File.exist?("public/images/wiki/#{file_name}.png")
+        html << "<img class=\"no-aa-image\" width=\"10%\" height=\"auto\" src=\"#{url("images/wiki/#{file_name}.png")}\"/>"
+      end
     end
     html << "</h1>"
-    html << "<p>"
+    #html << "<p>"
     WikiFiles.localized.invert.each do |key, value|
       if key != WikiFiles.localized[file_name.to_sym]
         files = Dir["views/wiki/**/*#{value}.*"]
-        file = url(files[0])
+        file  = url(files[0])
         file.sub!("views/", "")
         file.sub!(".qc", "")
         file_string.gsub! key, "<a href=\"#{file}\">#{key}</a>"
-        file_string.gsub! "\n", "<br/>"
+        #file_string.gsub! "\n", "<br/>"
       end
     end
     html << file_string
-    html << "</p>"
-    unless render_smelting(file_name.to_sym) == "" && 
-      render_crafting(file_name.to_sym) == ""
+    if file_name == 'index'
+      puts REDIS.get('readme')
+      html << GitHub::Markup.render('readme.md', REDIS.get('readme'))
+    end
+    #html << "</p>"
+    unless render_smelting(file_name.to_sym) == "" &&
+        render_crafting(file_name.to_sym) == ""
       html << "<br/><h2>Recipe</h2>"
     end
     html << render_smelting(file_name.to_sym)
